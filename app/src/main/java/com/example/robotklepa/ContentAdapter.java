f5f54f0c229132d36a68f5a80cb096a3e783b2ec
Максим_офис <mrbarawka@gmail.com>
2024-11-08 21:35:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/robotklepa/ContentAdapter.java
@@ -0,0 +1,46 @@
+package com.example.robotklepa;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ViewHolder>{
+    private final LayoutInflater inflater;
+    private final List<Content> contents;
+
+    ContentAdapter(Context context, List<Content> contents) {
+        this.contents = contents;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ContentAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.content_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ContentAdapter.ViewHolder holder, int position) {
+        Content content = contents.get(position);
+        holder.nameView.setText(content.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contents.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView nameView;
+        ViewHolder(View view) {
+            super(view);
+            nameView = view.findViewById(R.id.name);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/robotklepa/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.robotklepa;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String FILE_NAME = "contentList.txt";
+    private ArrayList<Content> contents = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText contentField = findViewById(R.id.contentInput);
+        Button addBtn = findViewById(R.id.addButton);
+        Button clearBtn = findViewById(R.id.clearButton);
+        setData();
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addData(contentField);
+                setData();
+                contentField.setText(null);
+            }
+        });
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearData();
+                //adapter.notifyItemInserted(contents.size());
+            }
+        });
+    }
+
+    private void addData(@NonNull EditText contentField) {
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_APPEND);
+            String contentName = contentField.getText().toString() + "\n";
+            fos.write(contentName.getBytes());
+            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void setData() {
+        FileInputStream fin = null;
+        updateContentList();
+        try {
+            fin = openFileInput(FILE_NAME);
+            contents.clear();
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String content = new String(bytes);
+            contents.add(new Content(content));
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void clearData() {
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            contents.clear();
+            updateContentList();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void updateContentList() {
+        RecyclerView recyclerView = findViewById(R.id.contentHolder);
+        ContentAdapter adapter = new ContentAdapter(this, contents);
+        recyclerView.setAdapter(adapter);
+    }
+}
